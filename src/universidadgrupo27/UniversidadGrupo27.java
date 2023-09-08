bbecb56a394f0c8bcc5f06abc385687ae27070ac
JorgeRJ85 <jorgeriverosjofre@gmail.com>
2023-09-08 11:08:44 -0300

    Cambios

--- src/universidadgrupo27/UniversidadGrupo27.java
+++ src/universidadgrupo27/UniversidadGrupo27.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadgrupo27;
 
-/**
- *
- * @author ilux
- */
+import java.sql.Connection;
+import universidadgrupo27.accesoADatos.Conexion;
+
+
 public class UniversidadGrupo27 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+       Connection con= Conexion.getConnection();
+        
+        
+        
     }
     
 }
---
+++ src/universidadgrupo27/accesoADatos/Conexion.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadgrupo27.accesoADatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ilux
+ */
+public class Conexion {
+     
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "universidadejemplo";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection;
+    
+    
+    private Conexion(){}
+    
+    public static Connection getConnection(){
+        if (connection==null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection=DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar driver");
+            } catch (SQLException ex) {
+                 JOptionPane.showMessageDialog(null, "Error al conectarse");
+            }
+            
+        }
+        return connection;
+    } 
+    
+    
+    
+    
+}
