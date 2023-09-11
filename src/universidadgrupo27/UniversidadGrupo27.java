662262798dde0993b1c4d32803fffe0b15e18f32
JorgeRJ85 <jorgeriverosjofre@gmail.com>
2023-09-11 10:31:57 -0300

    cambios

--- src/universidadgrupo27/UniversidadGrupo27.java
+++ src/universidadgrupo27/UniversidadGrupo27.java
@@ -18,13 +18,13 @@ public class UniversidadGrupo27 {
        //Alumno juan = new Alumno(12121212, "Sosa", "Juan Pascal", LocalDate.of(1991, 3 , 26), true);
        //Alumno luis = new Alumno(145789, "Bala", "Carlos", LocalDate.of(1995, 10, 15), true);
        
-       //AlumnoData alu = new AlumnoData();
+       AlumnoData alu = new AlumnoData();
        
        //alu.guardarAlumno(luis);
        
        //alu.modificarAlumno(juan);
        
-       //alu.eliminarAlumno(1);
+       alu.eliminarAlumno(1);
        
        //alu.buscarAlumno(2);
    
@@ -38,19 +38,19 @@ public class UniversidadGrupo27 {
        
        // **********Listando Los Alumnos activos de la Base de datos ******
        
-       AlumnoData alu = new AlumnoData();
-       
-        for (Alumno alumno : alu.listarAlumnos()) {
-            System.out.println("==============================");
-            System.out.println("DNI: "+alumno.getDni());
-            System.out.println("Apellido: "+alumno.getApellido());
-            System.out.println("Nombre: "+alumno.getNombre());
-            System.out.println("Fecha de Nacimiento: "+alumno.getFechaNac());
-            System.out.println("==============================");
-                  
-            
-            
-        }
+//       AlumnoData alu = new AlumnoData();
+//       
+//        for (Alumno alumno : alu.listarAlumnos()) {
+//            System.out.println("==============================");
+//            System.out.println("DNI: "+alumno.getDni());
+//            System.out.println("Apellido: "+alumno.getApellido());
+//            System.out.println("Nombre: "+alumno.getNombre());
+//            System.out.println("Fecha de Nacimiento: "+alumno.getFechaNac());
+//            System.out.println("==============================");
+//                  
+//            
+//            
+//        }
        
         
         
